c1d334a9f1bb27539bb84c45f4b7a2b50e6ecd85
Abhishek Chauhan <76480451+abhishekchauhan15@users.noreply.github.com>
2022-01-28 15:28:38 +0530

    updated yaml

--- routers/contest.js
+++ routers/contest.js
@@ -3,7 +3,7 @@ const router = new express.Router();
 const fs = require("fs");
 const fetch = require("node-fetch");
 const uuid = require("uuid");
-const ContestsDetails = require("../models/Schema");
+const ContestsDetails = require("../models/schema");
 const leetcodeContest = require("../models/leetcode_schema");
 var bodyParser = require("body-parser");
 
@@ -20,34 +20,6 @@ const apiCalling = async (url) => {
 
 //getting the contest details
 router.get("/contests", async (req, res) => {
-  const url = "https://kontests.net/api/v1/codeforces";
-  const data = await apiCalling(url);
-
-  try {
-    data.map((e, i) => {
-      const newContest = new ContestsDetails({
-        _id: uuid.v4(),
-        name: e.name,
-        url: e.url,
-        start_time: e.start_time,
-        end_time: e.end_time,
-        duration: e.duration,
-        in_24_hours: e.in_24_hours,
-        status: e.status,
-      });
-      newContest.save();
-      // console.log("saved");
-    });
-
-    const contestData = await ContestsDetails.find();
-    // res.json(contestData);
-    res.status(200).json(contestData);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-router.get("/contests/all", async (req, res) => {
   const url = "https://kontests.net/api/v1/all";
   const data = await apiCalling(url);
 
@@ -74,6 +46,123 @@ router.get("/contests/all", async (req, res) => {
     console.log(error);
   }
 });
+
+//creating the new contest in the database
+router.post("/contests", async (req, res) => {
+  try {
+    // console.log(req.body);
+    const newContest = new ContestsDetails(req.body);
+    const createContest = await newContest.save();
+
+    res.status(201).send(createContest);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+//get contest by id
+router.get("/contests/:id", async (req, res) => {
+  // console.log(req.params.id);
+  try {
+    const contestId = req.params.id;
+    console.log(contestId);
+    const contestData = await ContestsDetails.findById(contestId);
+
+    if (!contestData) {
+      return res
+        .status(404)
+        .send("The contest with the given ID was not found");
+    }
+
+    res.status(200).json(contestData);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//update contest by id by writing all the feilds + updated one also 
+router.put("/contests/:id", async (req, res) => {
+  try {
+    const contestId = req.params.id;
+    const contestData = await ContestsDetails.findByIdAndUpdate(
+      contestId,
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    if (!contestData)
+      return res
+        .status(404)
+        .send("The contest with the given ID was not found");
+
+    res.status(204).json(contestData);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//  get contest by name
+router.get("/contests/:name", async (req, res) => {
+  try {
+    const contestName = req.params.name;
+    // console.log(contestId);
+    const contestData = await ContestsDetails.findOne(contestName);
+
+    if (!contestData) {
+      return res
+        .status(404)
+        .send("The contest with the given ID was not found");
+    }
+
+    res.status(200).json(contestData);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// deleting the contest by id
+router.delete("/contests/:id", async (req, res) => {
+  try {
+    const contestId = req.params.id;
+    const contestData = await ContestsDetails.findByIdAndDelete(contestId);
+    if (!contestId)
+      return res
+        .status(404)
+        .send("The contest with the given ID was not found");
+
+    res.status(200).send("The contest with the given ID is deleted");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+
+//updating the contest by id py giving the update on particula feild
+router.patch("/contests/:id", async (req, res) => {
+  try {
+    const contestId = req.params.id;
+    const contestData = await ContestsDetails.findByIdAndUpdate(
+      contestId,
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    if (!contestData)
+      return res
+        .status(404)
+        .send("The contest with the given ID was not found");
+
+    res.status(204).json(contestData);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//---------------leetcode contest------------------
 
 router.get("/contests/leetcode", async (req, res) => {
   const url = "https://kontests.net/api/v1/leet_code";
@@ -107,8 +196,7 @@ router.get("/contests/leetcode", async (req, res) => {
 router.get("/contests/leetcode/:id", async (req, res) => {
   try {
     const contestId = req.params.id;
-    console.log(contestId);
-    console.log(contestId);
+    // console.log(contestId);
     const contestData = await leetcodeContest.findById(contestId);
 
     if (!contestData) {
@@ -116,99 +204,6 @@ router.get("/contests/leetcode/:id", async (req, res) => {
         .status(404)
         .send("The contest with the given ID was not found");
     }
-
-    res.status(200).json(contestData);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-//creating the new contest in the database
-router.post("/contests", async (req, res) => {
-  try {
-    // console.log(req.body);
-    const newContest = new ContestsDetails(req.body);
-    const createContest = await newContest.save();
-
-    res.status(201).send(createContest);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-//get contest by id
-router.get("/contests/:id ", async (req, res) => {
-  console.log(req.params.id);
-  try {
-    console.log(req.params.id);
-    const contestId = req.params.id;
-    console.log(contestId);
-    console.log(contestId);
-    const contestData = await ContestsDetails.findById(contestId);
-
-    if (!contestData) {
-      return res
-        .status(404)
-        .send("The contest with the given ID was not found");
-    }
-
-    res.status(200).json(contestData);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-//  get contest by name
-router.get("/contests/:name ", async (req, res) => {
-  try {
-    const contestName = req.params.name;
-    // console.log(contestId);
-    const contestData = await ContestsDetails.findOne(contestName);
-
-    if (!contestData) {
-      return res
-        .status(404)
-        .send("The contest with the given ID was not found");
-    }
-
-    res.status(200).json(contestData);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-// deleting the contest by id
-router.delete("/contests/:id", async (req, res) => {
-  try {
-    const contestId = req.params.id;
-    const contestData = await ContestsDetails.findByIdAndDelete(contestId);
-    if (!contestId)
-      return res
-        .status(404)
-        .send("The contest with the given ID was not found");
-
-    res.status(200).json(contestData);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-//updating the contest by id
-router.patch("/contests/:id", async (req, res) => {
-  try {
-    const contestId = req.params.id;
-    const contestData = await ContestsDetails.findByIdAndUpdate(
-      contestId,
-      req.body,
-      {
-        new: true,
-      }
-    );
-
-    if (!contestData)
-      return res
-        .status(404)
-        .send("The contest with the given ID was not found");
 
     res.status(200).json(contestData);
   } catch (error) {
